e89efcf9a08d6d4bceddd318edbd71275647d2aa
shaoyikai <shaoyikai@qq.com>
2018-05-08 08:06:42 +0800

    modify login.ui

--- login.cpp
+++ login.cpp
@@ -2,6 +2,7 @@
 #include "ui_login.h"
 #include "QString"
 #include "QDebug"
+#include "QMessageBox"
 
 Login::Login(QWidget *parent) :
     QDialog(parent),
@@ -20,12 +21,17 @@ void Login::on_btn_submit_clicked()
     QString account = ui->le_account->text();
     QString password = ui->le_password->text();
 
-    qDebug() << account;
-    qDebug() << password;
+    if(account.length() < 6 || password.length() < 8){
+        QMessageBox::critical(this,"错误","信息填写不完整，请重新检查","确定");
+    }else{
+        QString content = account + "\n" + password;
+        QMessageBox::question(this,"请确认信息",content,"确认","取消");
+    }
 }
 
 void Login::on_btn_clear_clicked()
 {
     ui->le_account->setText("");
     ui->le_password->setText("");
+    this->ui->le_account->setFocus();
 }
